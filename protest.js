d15944bcfd3d69cf0a77806b80ee13bdd9a98cbb
ignatius uwaoma <inspectorgeneral36@gmail.com>
2024-04-10 16:13:04 +0100

    trying out push undo

--- protest.js
+++ protest.js
@@ -4,3 +4,8 @@
 
 console.log("Hi git we are up and running");
 
+//secound day journey with git.
+
+console.log("Hello! git is exciting.");
+console.log("we dey kankpe");
+console.log(`two+one ={3}`);
---
+++ protest2.js
@@ -0,0 +1,14 @@
+//my second js file
+
+person={name:"obi",sex:"male",age:32};
+
+//log persons name
+console.log('my name '+person.name);
+
+//log persons age.
+
+console.log(person.name+' is'+person.age+' old');
+
+//log persons sex
+
+console.log(person.name+' is a '+perso.sex);
