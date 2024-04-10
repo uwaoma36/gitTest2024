1d8a30bce7acb8cf1376c72c141223172e1dc2b7
ignatius uwaoma <inspectorgeneral36@gmail.com>
2024-04-10 15:47:33 +0100

    ok

--- protest.js
+++ protest.js
@@ -4,3 +4,11 @@
 
 console.log("Hi git we are up and running");
 
+//secound day journey with git.
+
+console.log("Hello! git is exciting.");
+
+console.log("we now up and running.");
+
+console.log("it is done.");
+
---
+++ protest2.js
@@ -0,0 +1,13 @@
+//my second js file.
+
+person={name:"obi",age:35,sex:'male'};
+
+//log persons details
+
+console.log("persons name is "+person.name);
+
+console.log(person.name+" age is "+person.age);
+
+console.log(person.name+" is a "+person.sex);
+
+console.log("it is done.")
